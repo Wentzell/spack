1fc7cde9e7a024d90edfb661cca056ee77fdacb4
Gregory L. Lee <lee218@llnl.gov>
2017-06-26 10:32:39 -0700

    patch config.guess after autoreconf step

--- lib/spack/spack/build_systems/autotools.py
+++ lib/spack/spack/build_systems/autotools.py
@@ -95,10 +95,15 @@ class AutotoolsPackage(PackageBase):
     #: Options to be passed to autoreconf when using the default implementation
     autoreconf_extra_args = []
 
+    @run_after('autoreconf')
     def _do_patch_config_guess(self):
         """Some packages ship with an older config.guess and need to have
         this updated when installed on a newer architecture."""
 
+        if not self.patch_config_guess or not self.spec.satisfies(
+                'arch=linux-rhel7-ppc64le'
+        ):
+            return True
         my_config_guess = None
         config_guess = None
         if os.path.exists('config.guess'):
@@ -183,12 +188,6 @@ class AutotoolsPackage(PackageBase):
         :raise RuntimeError: if something goes wrong when patching
             ``config.guess``
         """
-
-        if self.patch_config_guess and self.spec.satisfies(
-                'arch=linux-rhel7-ppc64le'
-        ):
-            if not self._do_patch_config_guess():
-                raise RuntimeError('Failed to find suitable config.guess')
 
     @run_before('autoreconf')
     def delete_configure_to_force_update(self):
